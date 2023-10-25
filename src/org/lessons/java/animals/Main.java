4afbdff6c85f0c1a58dac95e9500b73af0dd4566
LorisMarzocchi <ml77182a@gmail.com>
2023-10-25 15:36:36 +0200

    Animals

--- src/org/lessons/java/animals/Main.java
+++ src/org/lessons/java/animals/Main.java
@@ -3,14 +3,14 @@ package org.lessons.java.animals;
 public class Main {
     public static void main(String[] args) {
 
-        Dog pippo = new Dog("pippo");
-        Dolphin flipper = new Dolphin("flipper");
-        Eagle freddy = new Eagle("freddy");
-        Sparrow sparrow = new Sparrow("pino");
+        Dog pippo = new Dog("Pippo");
+        Dolphin flipper = new Dolphin("Flipper");
+        Eagle freddy = new Eagle("Freddy");
+        Sparrow sparrow = new Sparrow("Pino");
 
         Animals[] animals = {pippo, flipper, freddy, sparrow};
         for (Animals animal : animals) {
-            System.out.println("mi chiamo: " + animal.getName());
+            System.out.println("My name is: " + animal.getName());
             animal.makeNoise();
             animal.eat();
             animal.sleep();
@@ -21,6 +21,7 @@ public class Main {
             if (animal instanceof CanSwim) {
                 faiNuotare((CanSwim) animal);
             }
+            System.out.println("*************** \n");
         }
 
     }
